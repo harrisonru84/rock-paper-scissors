a9aeac7629a19c28957fe5911037d92c4537a512
harrisonru84 <harrisonmrussell@gmail.com>
2023-09-17 12:53:14 -0700

    Fleshed out the pseudocode
    I worked out the majority of the algorithms for the problem before moving onto coding each of them.

---
+++ script.js
@@ -0,0 +1,17 @@
+// Pseudocode
+//
+// Create getComputerChoice variable randomly selects between 'rock,' 'paper,' and 'scissors'
+// Store the output from getComputerChoice in computerSelection
+// Ask player to input either 'rock,' 'paper,' or 'scissors' and store their response in playerSelection
+// Convert player's answer to all lowercase (if necessary)
+// If playerSelection is empty, display "You haven't chosen anything."
+// If playerSelection = rock & computerSelection = rock, display "Computer also chose rock. It's a tie."
+// If playerSelection = rock & computerSelection = scissors, display "Computer chose scissors. You win!"
+// If playerSelection = rock & computerSelection = paper, display "Computer chose paper. You lose."
+// If playerSelection = paper & computerSelection = rock, display "Computer chose rock. You win!"
+// If playerSelection = paper & computerSelection = paper, display "Computer also chose paper. It's a tie."
+// If playerSelection = paper & computerSelection = scissors, display "Computer chose scissors. You lose."
+// If playerSelection = scissors & computerSelection = rock, display "Computer chose rock. You lose."
+// If playerSelection = scissors & computerSelection = paper, display "Computer chose paper. You win."
+// If playerSelection = scissors & computerSelection = scissors, display "Computer also chose scissors. It's a tie."
+// Reset the game upon each player's selection.
