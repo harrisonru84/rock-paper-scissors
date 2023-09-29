69812f4cc62aad52618a2cd3fe93697aa46e733c
harrisonru84 <harrisonmrussell@gmail.com>
2023-09-29 15:13:02 -0700

    Beginning stage of adding ui
    I added a functioning UI to the rock paper scissors game, so it doesn't have to just be played in the console window. I created buttons for the player selection, and DOM manipulations that display the result and update the score as they go along. Once either the computer or the player's score reaches 5, it displays a message stating who won the game. Now trying to work out how to get the game to restart/reload once this happens, without the user having to reload the whole page themselves and preventing them from continuing to play once that score is reached.

--- script.js
+++ script.js
@@ -1,158 +1,113 @@
-// Pseudocode
+// Create getComputerChoice function randomly selects between 'rock,' 'paper,' and 'scissors'
+function getComputerChoice() {
+    let computerChoices = ["rock", "paper", "scissors"];
+    function getRandomInt() {
+        return Math.floor(Math.random() * 3);
+    }
+    return computerChoices[getRandomInt()];
+}
 
-// Breaks so that the console is visible underneath the Chrome prompt window
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("ROCK PAPER SCISSORS by HARRISON RUSSELL");
-console.log("\n");
+const rockButton = document.querySelector(".rock-button");
+const paperButton = document.querySelector(".paper-button");
+const scissorsButton = document.querySelector(".scissors-button");
 
-// Play through the game
+const roundResult = document.querySelector(".round-result");
+const playerResult = document.querySelector(".player-result");
+
+playerResult.textContent = "";
+roundResult.textContent = "";
+
+let computerWins = parseInt(0);
+let playerWins = parseInt(0);
+
+const score = document.querySelector(".score");
+const winner = document.querySelector(".winner");
+
+const playAgainButton = document.querySelector("play-again");
+playAgainButton.addEventListener('click', function() {location.reload()})
+
 game();
 
-// Create a game function that contains the entirety of the gameplay so it can be easily repeated
 function game() {
-    
-    // Create getComputerChoice function randomly selects between 'rock,' 'paper,' and 'scissors'
-    function getComputerChoice() {
-        // Array that stores choices
-        let computerChoices = ["rock", "paper", "scissors"];
-        // Random number generator that will select one of the choices
-        function getRandomInt() {
-            return Math.floor(Math.random() * 3);
-        }
-        return computerChoices[getRandomInt()];
+
+rockButton.addEventListener('click', playRock);
+paperButton.addEventListener('click', playPaper);
+scissorsButton.addEventListener('click', playScissors);
+
+function playRock() {
+    playerResult.textContent = "You chose rock.";
+    let playerSelection = "rock";
+    let computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        roundResult.textContent = "Computer also chose " + computerSelection + ". This round is a tie.";
     }
-
-    // Create two variables that score computer's and player's win count through each round
-    let computerWins = parseInt(0);
-    let playerWins = parseInt(0);
-
-    // Repeat the game five times
-    console.log("ROUND ONE")
-    playRound();
-    
-    console.log("ROUND TWO")
-    playRound();
-
-    console.log("ROUND THREE")
-    playRound();
-
-    console.log("ROUND FOUR")
-    playRound();
-
-    console.log("FINAL ROUND")
-    playRound();
-
-    // Create a function containing the gameplay for one full round, so it can be repeated 
-    function playRound() {
-        // Store the output from getComputerChoice in computerSelection
-        let computerSelection = getComputerChoice();
-
-        // Ask player to input either 'rock,' 'paper,' or 'scissors' and store their response in playerSelection
-        let playerSelection = prompt("Rock, paper, or scissors?");
-
-        // If playerSelection is empty, display "You haven't chosen anything."
-        if (playerSelection==="" || playerSelection===null || playerSelection.toLowerCase()!=="rock" && 
-            playerSelection.toLowerCase()!=="paper" && playerSelection.toLowerCase()!=="scissors") {
-            alert("You didn't enter anything, so you've foreited this round.");
-            break
-        }
-
-        else if (playerSelection===null) {
-            alert("You hit cancel, so you've forfeited this round. Computer wins by default.");
-            ++computerWins;
-            console.log("You forfeited, so the computer wins by default.");
-            console.log("Computer: " + computerWins + "\n" + "You: " + playerWins);
-            console.log("\n");
-        }
-
-        else if (playerSelection.toLowerCase()!=="rock" && playerSelection.toLowerCase()!=="paper" && playerSelection.toLowerCase()!=="scissors") {
-            alert("That wasn't one of the options. You've forfeited this round. Computer wins by default.")
-            ++computerWins;
-            console.log("You forfeited, so the computer wins by default.");
-            console.log("Computer: " + computerWins + "\n" + "You: " + playerWins);
-            console.log("\n");
-        }
-
-        else {
-            // Convert player's answer to all lowercase (if necessary)
-            playerSelection = playerSelection.toLowerCase();
-
-            // Display player's selection
-            console.log("You chose " + playerSelection + ".");
-
-            // If playerSelection = rock & computerSelection = scissors, display "Computer chose scissors. You win!"
-            if (playerSelection === "rock" && computerSelection === "scissors") {
-                // Player won, so increment player's win count by one
-                ++playerWins;
-                console.log("Computer chose scissors. You win!");
-            }
-            // If playerSelection = rock & computerSelection = paper, display "Computer chose paper. You lose."
-            else if (playerSelection === "rock" && computerSelection === "paper") {
-                // Player lost, so increment computer's win count by one
-                ++computerWins;
-                console.log("Computer chose paper. You lose.");
-            }
-            // If playerSelection = paper & computerSelection = rock, display "Computer chose rock. You win!"
-            else if (playerSelection === "paper" && computerSelection === "rock") {
-                ++playerWins;    
-                console.log("Computer chose rock. You win!");
-            }
-
-            // If playerSelection = paper & computerSelection = scissors, display "Computer chose scissors. You lose."
-            else if (playerSelection === "paper" && computerSelection === "scissors") {
-                ++computerWins;
-                console.log("Computer chose scissors. You lose.");
-            }
-            // If playerSelection = scissors & computerSelection = rock, display "Computer chose rock. You lose."
-            else if (playerSelection === "scissors" && computerSelection === "rock") {
-                ++computerWins;
-                console.log("Computer chose rock. You lose.");
-            }
-            // If playerSelection = scissors & computerSelection = paper, display "Computer chose paper. You win."
-            else if (playerSelection === "scissors" && computerSelection === "paper") {
-                ++playerWins;
-                console.log("Computer chose paper. You win.");
-            }
-            // If playerSelection = computerSelection "Computer also chose __________. It's a tie."
-            else if (playerSelection === computerSelection) {
-                console.log("Computer also chose " + computerSelection + ". It's a tie.");
-            }
-
-            console.log("Computer: " + computerWins + "\n" + "You: " + playerWins);
-            console.log("\n");
-
-        }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        roundResult.textContent = "Computer chose scissors. You win this round!";
+        ++playerWins;
     }
-
-    let result;
-
-    // Display the final score and tell the player whether or not they won
-    if (computerWins > playerWins) {
-        result = ("The computer won. Maybe you'll beat it next time.");
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        roundResult.textContent = "Computer chose paper. You lose this round.";
+        ++computerWins;
     }
-    else if (playerWins > computerWins) {
-        result = ("You won! You're smarter than a computer.");
+    score.textContent = "Computer: " + computerWins + " / You: " + playerWins;
+    if (computerWins === parseInt(5) && playerWins !== parseInt(5)) {
+        winner.textContent = "Computer wins. Try again next time."
     }
-    else if (playerWins ==="0" && computerWins ==="0") {
-        result = ("Neither you nor the computer scored. Maybe next time.")
+    else if (playerWins === parseInt(5) && computerWins !== parseInt(5)) {
+        winner.textContent = "You win. You're smarter than a computer!"
+
     }
-    else {
-        result = ("It was a tie. You're essentially as smart as a computer.");
+}
+
+function playPaper() {
+    playerResult.textContent = "You chose paper.";
+    let playerSelection = "paper";
+    let computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        roundResult.textContent = "Computer also chose " + computerSelection + ". This round is a tie.";
     }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        roundResult.textContent = "Computer chose rock. You win this round!";
+        ++playerWins;
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        roundResult.textContent = "Computer chose scissors. You lose this round.";
+        ++computerWins;
+    }
+    score.textContent = "Computer: " + computerWins + " / You: " + playerWins;
+    if (computerWins === parseInt(5) && playerWins !== parseInt(5)) {
+        winner.textContent = "Computer wins. Try again next time."
+    }
+    else if (playerWins === parseInt(5) && computerWins !== parseInt(5)) {
+        winner.textContent = "You win. You're smarter than a computer!"
 
-    // Display a game over message
-    console.log("Game over. \n \n" + result);
+    }
+}
 
-    console.log("\n");
+function playScissors() {
+    playerResult.textContent = "You chose scissors.";
+    let playerSelection = "scissors";
+    let computerSelection = getComputerChoice();
 
-    // Reset the game upon each player's selection.
-    console.log("Reload the page to try again.");
+    if (playerSelection === computerSelection) {
+        roundResult.textContent = "Computer also chose " + computerSelection + ". This round is a tie.";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        roundResult.textContent = "Computer chose paper. You win this round!";
+        ++playerWins;
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        roundResult.textContent = "Computer chose rock. You lose this round.";
+        ++computerWins;
+    }
+    score.textContent = "Computer: " + computerWins + " / You: " + playerWins;
+    if (computerWins === parseInt(5) && playerWins !== parseInt(5)) {
+        winner.textContent = "Computer wins. Try again next time."
+    }
+    else if (playerWins === parseInt(5) && computerWins !== parseInt(5)) {
+        winner.textContent = "You win. You're smarter than a computer!"
 
-}   
-
-
-
+    }
+}
